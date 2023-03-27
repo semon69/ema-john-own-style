ca5681ac142a32377996ac3bb167a1f9dea2dee1
semon69 <smdemon3@gmail.com>
2023-03-28 00:22:14 +0600

    set write value in UI from local storage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,16 +4,26 @@ const Cart = (props) => {
     const {cart} = props;
     let total = 0;
     let shipping = 0;
+    let quantity = 0;
     for(let product of cart){
-        total = total + product.price;
+        // general solution
+        // if(product.quantity === 0){
+        //     product.quantity = 1;
+        // } 
+
+        // shortcut solution 
+        product.quantity = product.quantity || 1;
+
+        total = total + product.price * product.quantity;
         shipping = shipping + product.shipping;
+        quantity = quantity + product.quantity;
     }
     const tax = parseFloat((total * 0.07).toFixed(2));
     const grandTotal = total + shipping + tax;
     return (
         <div>
             <h1 className='text-center text-3xl my-8'>Order Summery</h1>
-            <p className='mb-5'>Selected Item: {cart.length}</p>
+            <p className='mb-5'>Selected Item: {quantity}</p>
             <p className='mb-5'>Total Price: ${total}</p>
             <p className='mb-5'>Total Shipping Charge: ${shipping}</p>
             <p className='mb-5'>Tax: ${tax}</p>
